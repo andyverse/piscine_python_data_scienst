94a5268f5a1d578fcca1e4ba65d50a37fec3b84a
fnochiza <andy.ahmedov@gmail.com>
2022-09-18 00:04:26 +0300

    day01

---
+++ day01/ex00/data_types.py
@@ -0,0 +1,23 @@
+def data_types():
+
+    a = 1
+    b = 'Hello'
+    c = 1.2
+    d = True
+    e = ['1', '2']
+    f = {'name': 'mcherrie', 'location': 'at-g5'}
+    g = ('Hello', 1, True)
+    h = set()
+
+    print('[%s, %s, %s, %s, %s, %s, %s, %s]'
+          % (
+              type(a).__name__, type(b).__name__ ,
+              type(c).__name__, type(d).__name__,
+              type(e).__name__, type(f).__name__ ,
+              type(g).__name__, type(h).__name__
+            )
+          )
+
+
+if __name__ == '__main__':
+    data_types()
